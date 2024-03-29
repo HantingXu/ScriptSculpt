7915678a9bfaa2d000046e58f05c6cf7b77962b6
HantingXu <xhanting@seas.upenn.edu>
2024-03-29 10:24:08 -0400

    letter alignment partial

--- src/LetterAlignment.cpp
+++ src/LetterAlignment.cpp
@@ -5,6 +5,13 @@ LetterAlignment::LetterAlignment(std::vector<Letter>& letters, ImgShape& shape) 
 	this->shape = shape;
 }
 
+float LetterAlignment::positionalComp(const vec2& letter, const vec2& protrusion, int pathLength, int wordLength)
+{
+	vec2 diff = letter - protrusion;
+	float diff2 = diff.dot(diff);
+	return expf(- diff2 / powf(2.f * pathLength / wordLength, 2));
+}
+
 float LetterAlignment::orientationComp(const Anchor& anchor, const Protrusion& protrusion) {
 	int anchorType = anchor.type;
 	int protrusionType = protrusion.type;
